a586ef3418fb0da0e27ae2eddce95a158a761506
dev <kevin@itechaleart.com>
2025-11-11 14:28:42 +0530

    Remove image dimension validation checks from preview functions

--- public/admin/js/custom/image-preview.js
+++ public/admin/js/custom/image-preview.js
@@ -34,22 +34,22 @@ function preview86DimensionsFile(input) {
 
     var img = new Image();
     img.src = window.URL.createObjectURL(file);
-    img.onload = function () {
-        var width = this.naturalWidth,
-            height = this.naturalHeight;
+    // img.onload = function () {
+    //     var width = this.naturalWidth,
+    //         height = this.naturalHeight;
 
-        if (height !== 86) {
-            preview.src = "";
-            alert("Need to height is 86!");
-            return;
-        }
+    //     if (height !== 86) {
+    //         preview.src = "";
+    //         alert("Need to height is 86!");
+    //         return;
+    //     }
 
-        if (width !== 86) {
-            preview.src = "";
-            alert("Need to width is 86!");
-            return;
-        }
-    };
+    //     if (width !== 86) {
+    //         preview.src = "";
+    //         alert("Need to width is 86!");
+    //         return;
+    //     }
+    // };
 
     if (input.files[0].size > 1000000) {
         alert("Maximum file size is 1MB!");
@@ -79,22 +79,22 @@ function preview148DimensionsFile(input) {
 
     var img = new Image();
     img.src = window.URL.createObjectURL(file);
-    img.onload = function () {
-        var width = this.naturalWidth,
-            height = this.naturalHeight;
+    // img.onload = function () {
+    //     var width = this.naturalWidth,
+    //         height = this.naturalHeight;
 
-        if (height !== 148) {
-            preview.src = "";
-            alert("Need to height is 148!");
-            return;
-        }
+    //     if (height !== 148) {
+    //         preview.src = "";
+    //         alert("Need to height is 148!");
+    //         return;
+    //     }
 
-        if (width !== 148) {
-            preview.src = "";
-            alert("Need to width is 148!");
-            return;
-        }
-    };
+    //     if (width !== 148) {
+    //         preview.src = "";
+    //         alert("Need to width is 148!");
+    //         return;
+    //     }
+    // };
 
     if (input.files[0].size > 1000000) {
         alert("Maximum file size is 1MB!");
@@ -127,22 +127,22 @@ function preview505540DimensionFile(input) {
         var img = new Image();
 
         img.src = window.URL.createObjectURL(file);
-        img.onload = function () {
-            var width = this.naturalWidth,
-                height = this.naturalHeight;
+        // img.onload = function () {
+        //     var width = this.naturalWidth,
+        //         height = this.naturalHeight;
 
-            if (width !== 505) {
-                preview.src = "";
-                alert("Need to width is 505!");
-                return;
-            }
+        //     if (width !== 505) {
+        //         preview.src = "";
+        //         alert("Need to width is 505!");
+        //         return;
+        //     }
 
-            if (height !== 540) {
-                preview.src = "";
-                alert("Need to height is 540!");
-                return;
-            }
-        };
+        //     if (height !== 540) {
+        //         preview.src = "";
+        //         alert("Need to height is 540!");
+        //         return;
+        //     }
+        // };
 
         if (input.files[0].size > 1000000) {
             alert("Maximum file size is 1MB!");
@@ -179,22 +179,22 @@ function preview300343DimensionFile(input) {
         var img = new Image();
 
         img.src = window.URL.createObjectURL(file);
-        img.onload = function () {
-            var width = this.naturalWidth,
-                height = this.naturalHeight;
+        // img.onload = function () {
+        //     var width = this.naturalWidth,
+        //         height = this.naturalHeight;
 
-            if (width !== 300) {
-                preview.src = "";
-                alert("Need to width is 300!");
-                return;
-            }
+        //     if (width !== 300) {
+        //         preview.src = "";
+        //         alert("Need to width is 300!");
+        //         return;
+        //     }
 
-            if (height !== 343) {
-                preview.src = "";
-                alert("Need to height is 343!");
-                return;
-            }
-        };
+        //     if (height !== 343) {
+        //         preview.src = "";
+        //         alert("Need to height is 343!");
+        //         return;
+        //     }
+        // };
 
         if (input.files[0].size > 1000000) {
             alert("Maximum file size is 1MB!");
@@ -232,21 +232,21 @@ function preview12041DimensionFile(input) {
 
         img.src = window.URL.createObjectURL(file);
         console.log(window.URL.createObjectURL(file));
-        img.onload = function () {
-            var width = this.naturalWidth,
-                height = this.naturalHeight;
+        // img.onload = function () {
+        //     var width = this.naturalWidth,
+        //         height = this.naturalHeight;
 
-            if (width !== 120) {
-                alert("Need to width is 120!");
-                return;
-            }
+        //     if (width !== 120) {
+        //         alert("Need to width is 120!");
+        //         return;
+        //     }
 
-            if (height !== 41) {
-                preview.src = "";
-                alert("Need to height is 41!");
-                return;
-            }
-        };
+        //     if (height !== 41) {
+        //         preview.src = "";
+        //         alert("Need to height is 41!");
+        //         return;
+        //     }
+        // };
 
         if (input.files[0].size > 1000000) {
             alert("Maximum file size is 1MB!");
